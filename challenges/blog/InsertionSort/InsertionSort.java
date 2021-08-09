b7cb276b2e4413c3e90a3f9fc9d711b0d95662b5
Mahmoud-Saadeh <mahmoud.saadeh998@gmail.com>
2021-08-09 21:53:29 +0300

    merge sort

---
+++ challenges/blog/InsertionSort/InsertionSort.java
@@ -0,0 +1,28 @@
+package InsertionSort;
+
+class InsertionSort {
+//  public static void main(String[] args) {
+//    int arr[] = { 8,4,23,42,16,15 };
+//
+//    InsertionSort insertionSort = new InsertionSort();
+//    insertionSort.sort(arr);
+//
+//    for (int num: arr) {
+//      System.out.println(num);
+//    }
+//  }
+
+  void sort(int arr[]) {
+    int length = arr.length;
+    for (int i = 1; i < length; ++i) {
+      int key = arr[i];
+      int j = i - 1;
+
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+      }
+      arr[j + 1] = key;
+    }
+  }
+}
